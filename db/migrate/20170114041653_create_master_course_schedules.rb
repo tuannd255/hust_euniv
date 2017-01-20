fbc8b49f7b79018532933cbe1018933b4ef699dd
hatd <ennino1234@gmail.com>
2017-01-20 14:46:26 +0700

    edit database

--- db/migrate/20170114041653_create_master_course_schedules.rb
+++ db/migrate/20170114041653_create_master_course_schedules.rb
@@ -1,6 +1,7 @@
 class CreateMasterCourseSchedules < ActiveRecord::Migration[5.0]
   def change
     create_table :master_course_schedules do |t|
+
       t.string :code
       t.datetime :date
       t.integer :slot
