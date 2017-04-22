fe5ca5fbeafba09e7e9d910aae44c7662a009f58
hatd <ennino1234@gmail.com>
2017-04-22 10:07:01 +0700

    fix course create

--- app/models/master_subject.rb
+++ app/models/master_subject.rb
@@ -1,7 +1,8 @@
 class MasterSubject < ApplicationRecord
   has_many :master_class_subjects, dependent: :destroy
 
-  validates :code, presence:true
-  validates :name, presence:true
-  validates :creadit, presence:true
+  validates :code, presence: true, length: {maximum: 10}, uniqueness: true
+  validates :name, presence: true, length: {maximum: 70}
+  validates_numericality_of :creadit, presence: true, greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 10
 end
