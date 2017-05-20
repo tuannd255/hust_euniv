72f0fdd748f4aceb2dc3e8ffd1ffa2ccf5b0282d
hatd <ennino1234@gmail.com>
2017-05-20 13:31:35 +0700

    management-user

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 module HustEuniv
   class Application < Rails::Application
     config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    config.i18n.default_locale = :vi
+    config.middleware.use I18n::JS::Middleware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
